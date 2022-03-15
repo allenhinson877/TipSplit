37e9f309d0e96ce67c880971c95ea00cc904cd3d
allenhinson877 <58499378+allenhinson877@users.noreply.github.com>
2022-03-14 21:53:57 -0500

    Initial Commit

---
+++ TipSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TipSplit
+//
+//  Created by William Hinson on 3/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TipSplit/TipSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipSplitApp.swift
+//  TipSplit
+//
+//  Created by William Hinson on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct TipSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
